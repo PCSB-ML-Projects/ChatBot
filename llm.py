ff5c651c187abe46387af86700d288c0fd8cca9f
AetherVector <sakshihiremath97@gmail.com>
2025-02-05 15:16:16 +0530

    Updated files

--- llm.py
+++ llm.py
@@ -15,35 +15,38 @@ for i in range(len(docs)):
 
 embeddings = embeddings
 db = Chroma.from_documents(docs, embeddings)
-retriever = db.as_retriever(search_kwargs={"k": 3})
+retriever = db.as_retriever(search_kwargs={"k": 2})
 
 client = InferenceClient(api_key="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")
 
 def ask(query: str) -> str:
     ret = retriever.invoke(query)
-    system_prompt = f'''You are an AI assistant named XenoBot that helps people find information.
-    You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided.
-    You are a chatbot of PICT CSI CLUB which answer queries and recommend competitions of the club's flagship event "XENIA".
-    MAKE SURE YOU FULLY ANSWER THE QUERY and use indentation.
-    Give a generic answer if the query is not found in the data.
-    Data : <{ret[0].page_content}>,
-           <{ret[1].page_content}>,
-           <{ret[2].page_content}>     
+
+    system_prompt = f'''You are XenoBot, the AI assistant for PICT CSI CLUB which answer queries and recommend competitions of the club's flagship event "XENIA". 
+                        You are given the following extracted parts of a long document and a question.Answer queries accurately and concisely using only relevant details from the provided context.  
+
+                    - Use only information related to the query. Do not include details from other events.  
+                    - Give a generic answer if the QUERY IS NOT FOUND in the data.  
+                    - Keep responses structured and to the point and MAKE SURE YOU FULLY ANSWER THE QUERY and use indentation. 
+
+    Context Data:  <{ret[0].page_content}>,  
+                   <{ret[1].page_content}>  
     '''
+
     messages = [
         ("system", system_prompt),
         ("user", f"{query}"),
     ]
     message = client.chat_completion(
-        model="mistralai/Mixtral-8x7B-Instruct-v0.1",
+        model="mistralai/Mistral-7B-Instruct-v0.3",
         messages=messages,
         max_tokens=2000,
-        temperature=0.4,
+        temperature=0.3,
         stream=False
     )
     return message.choices[0].message.content
 
-# while 1:
-#     query = input("Ask query : ")
-#     response = ask(query)
-#     print(response)
+while 1:
+    query = input("Ask query : ")
+    response = ask(query)
+    print(response)
